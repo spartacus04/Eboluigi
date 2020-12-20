0379e08f0095176c96f14d54d60f903d6e97088f
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2020-12-20 20:38:42 +0100

    cambio link di l.whomademe e aggiorno dipendenze

--- commands/other/whomademe.js
+++ commands/other/whomademe.js
@@ -13,7 +13,7 @@ module.exports = class WhoMadeMeCommand extends Command {
 
   run(message) {
     message.say(
-      'Mio padre @spartacus04#9422 mi ha creato con :heart: Altre info a https://spartacus04.github.io/Eboluigi/'
+      'Mio padre @spartacus04#9422 mi ha creato con :heart: Altre info a https://github.com/spartacus04/Eboluigi'
     );
   }
 };
