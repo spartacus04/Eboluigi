9bc603c5f5c4b0a0d64d815b4d5cbb19b804c4c7
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2021-01-12 20:46:53 +0100

    Aggiungo l.ynet

---
+++ commands/other/ynet.js
@@ -0,0 +1,22 @@
+const { Command } = require('../../discord.js-commando/src');
+
+module.exports = class RandomNumberCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'ynet',
+      aliases: ['ynet'],
+      memberName: 'ynet',
+      group: 'other',
+      description: 'Invia ciuchino'
+    });
+  }
+
+  run(message) {
+    message.channel.send({
+        files: [{
+            attachment: "https://www.dropbox.com/s/4o4tndtc7uwxov0/ynet.jpg?dl=1",
+            name: 'ynet.png'
+        }]
+    })
+  }
+};
