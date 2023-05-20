5686ccb34aa5337b4d3cf639ecd71c09ce5cc9b4
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2023-05-20 09:37:25 +0200

    fix env loading

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import { config } from 'dotenv';
-config();
+(async () => {
+	try {
+		require.resolve('dotenv');
 
-(() => {
+		const { config } = await require('dotenv');
+		config();
+	}
+	catch(_) { null; }
+
 	import('./bot');
 })();
